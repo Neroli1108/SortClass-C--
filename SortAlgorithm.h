05c61651e1e24dcb879b8ad5c01f6570a88e9f07
Neroli <Neroli1108@gmail.com>
2017-09-07 04:12:47 -0700

    add two new sort
    add selection sort and insertion sort

--- SortAlgorithm.h
+++ SortAlgorithm.h
@@ -32,5 +32,39 @@ namespace sort {
 }
 
 namespace sort {
+	template<typename T, typename A>
+	void selectionSort(std::vector<T, A>&v) {
+		T min = v[0];
+		int i = 0, minIndex = -1;
+		while (i != v.size() - 1) {
+			int j = i + 1;
+			while (j != v.size()) {
+				if (v[j] < min) {
+					min = v[j];
+					minIndex = j;
+				}
+				j++;
+			}
+			std::swap(v[i], v[minIndex]);
+			i++;
+			min = v[i];
+		}
+	}
+}
 
+namespace sort {
+	template<typename T, typename A>
+	void InsertionSort(std::vector<T, A>&v) {
+		int i = 0;
+		while (i != v.size()) {
+			T tmp = v[i];
+			int j = i - 1;
+			while (j >= 0 && v[j] > tmp) {
+				v[j + 1] = v[j];
+				j--;
+			}
+			v[j + 1] = tmp;
+			i++;
+		}
+	}
 }
